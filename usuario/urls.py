83d92fe88e86fa5a1a1b042a1ea422c71a3d0185
bravodan23 <bdaniel@unicauca.edu.co>
2020-04-28 10:53:05 -0500

    Codigo irrelevante eliminado

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignupView
 
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='usuario/login.html'), name='usuario.login'),
